1120c69bfa1606936998667add9acec277f9c7e8
Lucas Tito <lucassga500@gmail.com>
2022-08-22 16:14:40 -0300

    can't build yet

--- Software_para_Dispositivos_Moveis/Trabalho1/README.md
+++ Software_para_Dispositivos_Moveis/Trabalho1/README.md
@@ -1,4 +1,5 @@
 <h1>Descrição</h1>
 
-+ Criar um app que não necessariamente tenha sentido e que possui uma série de conteúdos
-+ Também criei um tema agradável para o app
+
+
+Criar um app que não necessariamente tenha sentido e que possui uma série de conteúdos
--- Software_para_Dispositivos_Moveis/Trabalho1/app/src/main/java/com/example/trabalho1/MainActivity.java
+++ Software_para_Dispositivos_Moveis/Trabalho1/app/src/main/java/com/example/trabalho1/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.trabalho1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] dropdown_menu = {"somos", "elementos", "do array"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spiner = findViewById(R.id.dropdown_menu);
+        spiner.setOnItemSelectedListener(this);
+
+        //converte o array em elementos do dropdown
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, dropdown_menu);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiner.setAdapter(aa);
+        //-----------------------------------------
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, dropdown_menu[i], Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
