1ba12109ef1ced91d577749db37d06cddc1e36d7
Lucas Tito <lucassga500@gmail.com>
2022-08-27 20:33:37 -0300

    implemented a song button on second screen

--- Software_para_Dispositivos_Moveis/Trabalho1/app/src/main/java/com/example/trabalho1/SecondScreen.java
+++ Software_para_Dispositivos_Moveis/Trabalho1/app/src/main/java/com/example/trabalho1/SecondScreen.java
@@ -2,9 +2,16 @@ package com.example.trabalho1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondScreen extends AppCompatActivity {
+
+    MediaPlayer oldie_song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +19,14 @@ public class SecondScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
 
+        build_play_btn();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        oldie_song.release();
     }
 
     @Override
@@ -22,7 +37,20 @@ public class SecondScreen extends AppCompatActivity {
 
     }
 
-    private void playSound(){
+
+    private void build_play_btn() {
+
+        Button playBtn = findViewById(R.id.play_btn);
+        oldie_song = MediaPlayer.create(this, R.raw.oldie_song);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                oldie_song.start();
+
+            }
+        });
+
 
     }
 }
