017ab5cc9a35a752d699db5d968b120917b0e52b
Lucas Tito <lucassga500@gmail.com>
2022-08-31 13:13:36 -0300

    translated to english and added README

--- Programming_Fundamentals/Semana_Struct/PointInsideCircle/PointInsideCirlcle.c
+++ Programming_Fundamentals/Semana_Struct/PointInsideCircle/PointInsideCirlcle.c
@@ -2,18 +2,15 @@
 #include<math.h>
 
 typedef struct{
-    
-    float x, y, raio;
-    
-    
-}Circulo;
+    float x, y, radius;
+}Circle;
 
 
-int distanceChecker(Circulo c1, Circulo c2){
+int distanceChecker(Circle c1, Circle c2){
     
     float result = sqrt( pow((c2.x-c1.x),2) + pow((c2.y-c1.y),2) );
     
-    if(result<=c1.raio) return 1;
+    if(result<=c1.radius) return 1;
     else return 0;
     
 }
@@ -21,18 +18,16 @@ int distanceChecker(Circulo c1, Circulo c2){
 
 int main(){
     
-    Circulo c;
-    scanf("%f %f %f", &c.x, &c.y, &c.raio);
+    Circle c;
+    scanf("%f %f %f", &c.x, &c.y, &c.radius);
     
-    Circulo ToCheck;
+    Circle ToCheck;
     scanf("%f %f", &ToCheck.x, &ToCheck.y);
     
     int IsIn = distanceChecker(c, ToCheck);
     
     if(IsIn) printf("1");
-    else printf("0");
-    
-    
+    else printf("0");    
     
     return 0;
 }
