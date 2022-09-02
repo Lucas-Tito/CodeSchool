f96133ce76b553de5dfb8b777e1f6044143c28a2
Lucas Tito <lucassga500@gmail.com>
2022-09-02 00:52:16 -0300

    solved conflict

--- Programming_Fundamentals/RecursiveFunctionNFunctions_Week/PowRecursiveFunction/pow_recursive_function.c
+++ Programming_Fundamentals/RecursiveFunctionNFunctions_Week/PowRecursiveFunction/pow_recursive_function.c
@@ -13,19 +13,11 @@ int calcPow(int base, int power){
 
 int main(){
     
-<<<<<<<< HEAD:Programming_Fundamentals/SemanaFunções_+_FunçõesRecursivas/RecursiveFunctionPow/recursive_function_pow.c
-    printf("Inform base and power: ");
-    int base, power;
-    scanf("%d %d", &base, &power);
-    
-    printf("\nThis is the result: ");
-========
     printf("Enter base and power: \n");
     int base, power;
     scanf("%d %d", &base, &power);
     
     printf("The result is: ");
->>>>>>>> 16b5f7adcce502a70a0eb51664f76bfab63f9a27:Programming_Fundamentals/RecursiveFunctionNFunctions_Week/PowRecursiveFunction/pow_recursive_function.c
     printf("%d", calcPow(base, power));
     
     
