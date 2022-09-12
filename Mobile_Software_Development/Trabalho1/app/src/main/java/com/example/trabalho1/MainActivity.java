e3277719b8c20a15317518def02288d11961f62f
Lucas Tito <lucassga500@gmail.com>
2022-09-12 12:54:23 -0300

    finally got custom theme to work
    for a still unknown reason, night/theme wasn't working, but when a tried using the normal themes.xml it worked beautifully

--- Mobile_Software_Development/Trabalho1/app/src/main/java/com/example/trabalho1/MainActivity.java
+++ Mobile_Software_Development/Trabalho1/app/src/main/java/com/example/trabalho1/MainActivity.java
@@ -1,40 +1,16 @@
 package com.example.trabalho1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.content.pm.PackageManager;
-import android.content.Context;
-import android.content.Intent;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
-import android.widget.TableLayout;
 import android.widget.Toast;
-import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
