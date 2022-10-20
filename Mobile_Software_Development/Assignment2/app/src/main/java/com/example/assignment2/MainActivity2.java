d7d1739b70471e43aa21516d857f6ea5b18d3f76
Lucas Tito <lucassga500@gmail.com>
2022-10-20 15:34:22 -0300

    changes to get a cleaner code

--- Mobile_Software_Development/Assignment2/app/src/main/java/com/example/assignment2/MainActivity2.java
+++ Mobile_Software_Development/Assignment2/app/src/main/java/com/example/assignment2/MainActivity2.java
@@ -50,7 +50,7 @@ public class MainActivity2 extends AppCompatActivity {
         }
         else if(getIntent().getIntExtra("fragToStart", 0) == 2){
             int carID = getIntent().getIntExtra("carID", 0);
-            fragmentToStart = activity2_edit_car.newInstance(carDAO, carID);
+            fragmentToStart = activity2_edit_car.newInstance(carDAO, carID );
         }
 
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragmentToStart).commit();
